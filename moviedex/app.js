97ef4ae49f70f645b9fd6e00c4e8c7ea2868474f
AReach <DerAReich@gmail.com>
2019-02-21 14:45:28 -0600

    minor bug fixes for country filter and avgvote filter

--- moviedex/app.js
+++ moviedex/app.js
@@ -15,13 +15,13 @@ app.use(morgan('dev'));
 app.use(function validateBearerToken(req, res, next) {
   let bearerToken = req.get('Authorization');
   if (!bearerToken) {
-    return res.status(401).json({error: 'Please provide your bearer API_KEY'});
+    return res.status(401).json({message: 'Please provide your bearer API_TOKEN'});
   }
   bearerToken = bearerToken.slice(bearerToken.indexOf(' ')+1);
   const apiToken = process.env.API_TOKEN;
 
   if (!apiToken && bearerToken !== apiToken) {
-    return res.status(401).json({error: 'Unauthorized request'});
+    return res.status(401).json({message: 'Unauthorized request'});
   }
   next();
 });
@@ -34,13 +34,13 @@ function handleGetMovies(req, res) {
   }
 
   if (req.query.country){
-    response = response.filter(item => item.county.toLowerCase().includes(req.query.country.toLowerCase()));
+    response = response.filter(item => item.country.toLowerCase().includes(req.query.country.toLowerCase()));
   }
 
   if (req.query.avgvote) {
     const queryAvgVote = parseFloat(req.query.avgvote);
-    if (queryAvgVote.isNan()) {
-      res.status(400).send('Supplied avg. vote value must be a number');
+    if (isNaN(queryAvgVote)) {
+      res.status(400).json({message: 'Make sure the supplied average number is actually a number!'});
     }
     response = response.filter(item => item.avg_vote >= queryAvgVote);
   }
